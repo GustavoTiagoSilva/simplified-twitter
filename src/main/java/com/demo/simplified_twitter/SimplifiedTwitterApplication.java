d17340e19f459b29b5f55fdfbab22f96a0b48825
Gustavo Silva <Gustavo_Silva@McKinsey.com>
2024-11-02 12:55:16 -0300

    Inital Commit

---
+++ src/main/java/com/demo/simplified_twitter/SimplifiedTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.demo.simplified_twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/simplified_twitter/SimplifiedTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.simplified_twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
