32eecbccf1d62a2ba6b371a4ff360d30535ae798
Gustavo Silva <Gustavo_Silva@McKinsey.com>
2024-11-11 15:30:52 -0300

    refactor: removing unused methods

--- src/main/java/com/demo/simplified_twitter/dto/JwtDto.java
+++ src/main/java/com/demo/simplified_twitter/dto/JwtDto.java
@@ -1,9 +1,4 @@
 package com.demo.simplified_twitter.dto;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 public record JwtDto(String accessToken, Long expiresIn) {
-    public boolean isLoginCorrect(PasswordEncoder passwordEncoder, LoginRequestDto loginRequest, String password) {
-        return passwordEncoder.matches(loginRequest.password(), password);
-    }
 }
